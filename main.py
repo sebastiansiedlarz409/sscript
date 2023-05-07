500e63fa2216e58ebbc528bd078269e30a124166
Sebastian Siedlarz <siedlarzsebastian409@gmail.com>
2023-05-07 15:18:06 +0200

    Prepare scope for variables

--- main.py
+++ main.py
@@ -6,24 +6,47 @@ lexer = SSLexer()
 parser = SSParser()
 runtime = SSRuntime()
 
-#open source file
+# here i test scoping
+# from runtime.ssscope import SSRuntimeScope
+# from runtime.values import *
+# mainScope = SSRuntimeScope()
+# av = NumberRuntimeValue()
+# av.setValue(10)
+# bv = NumberRuntimeValue()
+# bv.setValue(22)
+# mainScope.declareValueSymbol("a", av)
+# mainScope.declareValueSymbol("b", bv)
+# print(mainScope.peakValueSymbol("a"))
+# print(mainScope.peakValueSymbol("b"))
+# mainScope.assignValueSymbol("a", bv)
+# print(mainScope.peakValueSymbol("a"))
+# childScope = SSRuntimeScope()
+# childScope.setParentScope(mainScope)
+# #childScope.declareValueSymbol("a", av) #ok, failed
+# print(childScope.peakValueSymbol("a"))
+# childScope.assignValueSymbol("a", av)
+# print(mainScope.peakValueSymbol("a"))
+# print(childScope.peakValueSymbol("a"))
+# exit()
+
+# open source file
 source = ""
 with open("_s3.ss", "r") as f:
     source = f.read()
 
-#tokenize
+# tokenize
 print("Lexer:")
 tokens = lexer.tokenize(source)
 for t in tokens: print(t)
 print()
 
-#parse
+# parse
 print("Parser:")
 program = parser.parseProgram(tokens)
 print(program)
 print()
 
-#runtime
+# runtime
 print("Runtime:")
 result = runtime.execute(program)
 print(result)
---
+++ runtime/ssscope.py
@@ -0,0 +1,85 @@
+#every scope has list of identifiers
+#identifier can be value type (variables, consts) or function type (functions)
+
+from runtime.values import *
+
+class RuntimeIdentifier:
+    def __init__(self):
+        self.identifier: str = None
+
+    def setIdentifier(self, identifier: str):
+        self.identifier = identifier
+
+class ValueRuntimeIdentifier(RuntimeIdentifier):
+    def __init__(self):
+        self.value: RuntimeValue = None
+
+    def setValue(self, value: RuntimeValue):
+        self.value = value
+
+    def __repr__(self) -> str:
+        ret = f"{self.identifier} = {self.value}"
+        return ret
+    
+class FunctionRuntimeIdentifier(RuntimeIdentifier):
+    def __init__(self):
+        pass
+
+    #TODO: function
+    
+#scope
+class SSRuntimeScope:
+    def __init__(self): 
+        #parent in type Runtimescope
+        self.parent = None
+        self.symbols: list[RuntimeIdentifier] = []
+
+    def setParentScope(self, parent):
+        self.parent = parent
+
+    #check if symbol exist in myself or in my ancestor
+    #if exist returns it
+    #otherwise returns None
+    def checkIfSymbolExists(self, symbol: str) -> RuntimeIdentifier:
+        test = [x for x in self.symbols if x.identifier == symbol]
+        if len(test) == 1:
+            return test[0]
+        
+        if self.parent != None:
+            return self.parent.checkIfSymbolExists(symbol)
+        
+        return None
+
+    #alway declare inside myself
+    def declareValueSymbol(self, symbol: str, value: RuntimeValue):
+        #check if already symbol exists
+        if self.checkIfSymbolExists(symbol) != None:
+            raise Exception(f"SSRuntime: Identifier '{symbol}' has already been declared")
+        
+        s = ValueRuntimeIdentifier()
+        s.setIdentifier(symbol)
+        s.setValue(value)
+
+        self.symbols.append(s)
+    
+    #override can reassign in myself or in my ancestor
+    def assignValueSymbol(self, symbol: str, value: RuntimeValue):
+        #check if already symbol exists
+        s = self.checkIfSymbolExists(symbol)
+        if s == None:
+            raise Exception(f"SSRuntime: Identifier '{symbol}' has not been declered yet")
+
+        #override 
+        s.setValue(value)
+
+    #return symbol value
+    def peakValueSymbol(self, symbol: str) -> RuntimeValue:
+        #check if already symbol exists
+        s = self.checkIfSymbolExists(symbol)
+        if s == None:
+            raise Exception(f"SSRuntime: Identifier '{symbol}' has not been declered yet")
+        
+        return s.value
+
+
+    
